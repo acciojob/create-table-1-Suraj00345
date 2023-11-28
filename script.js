c1e00cef8ee35f65dfcb3699b76005bb4e631cdd
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-11-28 18:56:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ function insert_Row() {
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   cell1.innerHTML = "New Cell1";
-  cell2.innerHTML = "New cell2";
+  cell2.innerHTML = "New Cell2";
 }
